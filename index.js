a4e800c75f74ae50a05e6f7fe64a4552f11222af
Gnv Saikiran <gajjarapusai1@gmail.com>
2020-10-31 12:08:13 +0530

    Add select for selecting type of color

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ function sketch(gridSize) {
         child.style.border = "1px solid teal";
         container.appendChild(child);
         child.addEventListener('mouseover', () => {
-            child.style.backgroundColor = "black";
+            let randomColor = Math.floor(Math.random() * 16777216).toString(16);
+            if (document.querySelector('select').value === "random") {
+                child.style.backgroundColor = `#${randomColor}`;
+            } else {
+                child.style.backgroundColor = "black";
+            }
         })
     }
 }
@@ -22,5 +27,9 @@ function resetGrid() {
     container.innerHTML = "";
 
     let newGridSize = prompt("Enter new grid size(1-100):");
-    sketch(newGridSize);
+    if (newGridSize >= 1 && newGridSize <= 100) {
+        sketch(newGridSize);
+    } else {
+        resetGrid();
+    }
 }
