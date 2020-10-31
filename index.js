c74d412c2ad36def2ba86948d60f739f902a5def
Gnv Saikiran <gajjarapusai1@gmail.com>
2020-10-31 11:18:12 +0530

    refactored code

--- index.js
+++ index.js
@@ -4,20 +4,12 @@ function sketch(gridSize) {
     container.style.setProperty('grid-template-rows', `repeat(${gridSize}, ${(1 / gridSize) * 600}px)`);
     container.style.setProperty('grid-template-columns', `repeat(${gridSize}, ${(1 / gridSize) * 650}px)`);
 
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            const child = document.createElement('div');
-            child.style.border = "1px solid teal";
-            child.classList.add('grid-element');
-            container.appendChild(child);
-        }
-    }
-
-    let grids = document.querySelectorAll('div[class="grid-element"]');
-
     for (let i = 0; i < gridSize * gridSize; i++) {
-        grids[i].addEventListener('mouseover', () => {
-            grids[i].style.backgroundColor = "black";
+        const child = document.createElement('div');
+        child.style.border = "1px solid teal";
+        container.appendChild(child);
+        child.addEventListener('mouseover', () => {
+            child.style.backgroundColor = "black";
         })
     }
 }
